8b59d7feb19290aa63a6902bdbc284d0f5e5ee5c
franco-jyq <fgazzola@fi.uba.ar>
2024-04-01 19:15:03 -0300

    Feat: Added middleware with firebase authentication

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { createUserRouter } from "./routes/users.routes.js";
+import { verifyFirebaseToken } from "./middlewares/firebase.auth.js";
 
 export const createApp = () => {
   const app = express();
-
+  
   //middlewares
   app.use(express.json());
-
+    
+  app.use(verifyFirebaseToken);
+    
   app.use(createUserRouter());
-
+  
   return app;
 };
+
---
+++ src/middlewares/firebase.auth.js
@@ -0,0 +1,29 @@
+import { applicationDefault, initializeApp } from 'firebase-admin/app';
+import {getAuth} from 'firebase-admin/auth';
+
+initializeApp({
+    credential: applicationDefault(),
+  });
+  
+
+export const verifyFirebaseToken = async (req, res, next) => {
+
+    if (process.env.NODE_ENV === 'test') {
+        return next();
+    }
+    const idToken = req.headers.authorization;
+
+    if (!idToken) {
+        return res.status(401).send('Unauthorized');
+    }
+
+    try {
+        const decodedToken = await getAuth().verifyIdToken(idToken);
+        req.user = decodedToken; // Attach the decoded token to the request object for further use
+        next(); 
+    } catch (error) {
+        console.error('Error verifying Firebase token:', error);
+        return res.status(403).send('Forbidden');
+    }
+};
+
