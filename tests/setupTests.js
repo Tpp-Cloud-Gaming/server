0de9474f9bce697f6ac9072f3c63fe22e6626bd7
franco-jyq <fgazzola@fi.uba.ar>
2024-04-05 17:43:41 -0300

    Fix: added missing timeout from previous refactor

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -3,7 +3,7 @@ import { startContainer, stopContainer } from "./dbSetup.js";
 
 beforeAll(async () => {
   await startContainer();
-});
+}, 30000);
 
 afterAll(async () => {
   await stopContainer();
