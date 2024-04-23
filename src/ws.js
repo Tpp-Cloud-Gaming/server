84d295818be7d320e17d407efacd31ba207aad7a
franco-jyq <fgazzola@fi.uba.ar>
2024-04-23 14:27:59 -0300

    Separate ws logic into folders webrtcHandshake and sessionHandling.

--- src/ws.js
+++ src/ws.js
@@ -2,14 +2,15 @@
 import { WebSocketServer } from "ws";
 import { createServer } from "http";
 import { app } from "./index.js";
-import { UserGame } from "./models/UserGame.js";
+import {offererSdp,clientSdp, initOfferer, initClient} from "./ws/webrtcHandshake.js";
+import { getUsersForGames } from "./ws/sessionHandling.js";
 
-// Define the port number for the server
-const PORT = process.env.PORT || 3000;
 
 // Create a WebSocket server instance
+const PORT = process.env.PORT || 3000;
 const httpServer = createServer();
 const wss = new WebSocketServer({ server: httpServer });
+
 httpServer.on("request", app);
 httpServer.listen(PORT, () => {
   console.log(`WebSocket server is running on port ${PORT}`);
@@ -17,7 +18,7 @@ httpServer.listen(PORT, () => {
 
 let connectedOfferers = {};
 let connectedClients = {};
-// Handle new client connections
+
 wss.on("connection", (ws) => {
   console.log("A new client connected.");
   ws.on("message", async (message) => {
@@ -29,78 +30,28 @@ wss.on("connection", (ws) => {
   });
 });
 
-//console.log(`WebSocket server is running on port ${PORT}`);
-
 async function handleMessage(message, ws) {
   const messageFields = message.toString().split("|");
-  // messageType | .....
   const messageType = messageFields[0];
-
   switch (messageType) {
+    
     case "initOfferer":
-      // initOfferer|usernameOfferer
-      var usernameOfferer = messageFields[1];
-      connectedOfferers[usernameOfferer] = ws;
-      console.log("initOfferer with username: " + usernameOfferer);
+      initOfferer(ws, messageFields, connectedOfferers);
       break;
-    case "initClient":
-      // initClient|usernameClient|usernameOfferer|gameName
-      var usernameClient = messageFields[1];
-      var usernameOfferer = messageFields[2];
-      var gameName = messageFields[3];
-      connectedClients[usernameClient] = ws;
 
-      if (connectedOfferers[usernameOfferer]) {
-        connectedOfferers[usernameOfferer].send(
-          `sdpRequestFrom|${usernameClient}|${gameName}`,
-        );
-      } else {
-        ws.send("Offerer not found"); // TODO: definir un mensaje especifico
-      }
+    case "initClient":
+      initClient(ws, messageFields, connectedClients, connectedOfferers);
       break;
 
     case "offererSdp":
-      // offererSdp|usernameClient| <sdp>
-      var usernameClient = messageFields[1];
-      var sdpOfferer = messageFields[2];
-      if (connectedClients[usernameClient]) {
-        connectedClients[usernameClient].send(`sdpOfferer|${sdpOfferer}`);
-      } else {
-        ws.send("Client not found");
-      }
+      offererSdp(ws, messageFields, connectedClients);
       break;
 
-    case "clientSdp":
-      // clientSdp|usernameOfferer| <sdp>
-      var usernameOfferer = messageFields[1];
-      var sdpClient = messageFields[2];
-      if (connectedOfferers[usernameOfferer]) {
-        connectedOfferers[usernameOfferer].send(`sdpClient|${sdpClient}`);
-      } else {
-        ws.send("Offerer not found");
-      }
+    case "clientSdp":        
+      clientSdp(ws, messageFields, connectedOfferers);
       break;
 
-    case "getUsersForGames":
-      // getUsersForGames
-      var gameName = messageFields[1];
-      // Consulta  a la base de datos
-      const possibleUsers = await UserGame.findAll({
-        where: { gamename: gameName },
-        attributes: ["username"],
-      }).then((data) => {
-        return data.map((entity) => entity.get("username"));
-      });
-
-      let offerers = "";
-      for (let connectedUser in connectedOfferers) {
-        for (var i = 0; i < possibleUsers.length; i++) {
-          if (connectedUser === possibleUsers[i]) {
-            offerers += "|" + connectedUser;
-          }
-        }
-      }
-      console.log(offerers);
-      ws.send("usersForGame" + offerers);
-  }
+    case "getUsersForGames":      
+      getUsersForGames(ws, messageFields, connectedOfferers);
+}
 }
---
+++ src/ws/sessionHandling.js
@@ -0,0 +1,24 @@
+import { UserGame } from "../models/UserGame.js";
+
+
+export async function getUsersForGames(ws, messageFields, connectedOfferers) {
+    var gameName = messageFields[1];
+    // Consulta  a la base de datos
+    const possibleUsers = await UserGame.findAll({
+      where: { gamename: gameName },
+      attributes: ["username"],
+    }).then((data) => {
+      return data.map((entity) => entity.get("username"));
+    });
+
+    let offerers = "";
+    for (let connectedUser in connectedOfferers) {
+      for (var i = 0; i < possibleUsers.length; i++) {
+        if (connectedUser === possibleUsers[i]) {
+          offerers += "|" + connectedUser;
+        }
+      }
+    }
+    console.log(offerers);
+    ws.send("usersForGame" + offerers);
+}
